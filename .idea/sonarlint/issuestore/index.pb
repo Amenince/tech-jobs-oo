185ad0ac63dafa21d770506f1078419c0c78ea5a
anthony_cook_ <amenince777@gmail.com>
2020-07-16 19:22:38 -0500

    updated unit test

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,17 @@
 
 9
 	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+X
+(src/org/launchcode/techjobs_oo/Main.java,c\e\ce8661853cc7c3701c9296c120d686674c22f035
+b
+2src/org/launchcode/techjobs_oo/CoreCompetency.java,7\9\79fce92c9d78cd7beb72849e3f9054a2d6e508b5
+\
+,src/org/launchcode/techjobs_oo/Employer.java,8\f\8fdc6bd64c97f1a7eba8ebc275c11f194638ecfd
+\
+,src/org/launchcode/techjobs_oo/Location.java,c\e\ce03e83d09f0928d00b5b418c31c1c6c6dd113a3
+`
+0src/org/launchcode/techjobs_oo/PositionType.java,f\2\f24cb63502090db4a01268f113b536b69853b015
+W
+'src/org/launchcode/techjobs_oo/Job.java,e\c\ec4dca94103d64b7c8a5b5125ecb4b1db8e978e5
+F
+src/Tests/JobTest.java,f\3\f3498aa297919090b900a1d270e322cd65d3cd4d
--- src/Tests/JobTest.java
+++ src/Tests/JobTest.java
@@ -1,22 +1,39 @@
 package Tests;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.launchcode.techjobs_oo.Job;
+import org.launchcode.techjobs_oo.*;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 
 public class JobTest {
+    Job first_job;
+    Job second_job;
+    Job third_job;
 
     @Before
-    public void newJobObj() {
-        first_job = new Job();
-        second_job = new Job();
+    public void createJobObject() {
+        first_job = new Job(1, "Steve", new Employer("McDonalds"), new Location("St. Louis"), new PositionType("Cook"), new CoreCompetency("Stuff"));
+        second_job = new Job(1, "Bill", new Employer("Burger King"), new Location("St. Louis"), new PositionType("Manager"), new CoreCompetency("Other"));
+        third_job = new Job(1,"Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+
     }
 
     @Test
     public void testSettingJobId() {
-        assertEquals(1,);
+        assertEquals("Oops, Id's are equal!", first_job.getId(), second_job.getId(), 1);
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        assertEquals("No Match!", third_job.getId(), 1, 1);
+        assertEquals("No Match!", third_job.getName(), "Product tester");
+        assertEquals("No Match", third_job.getEmployer(), "ACME");
+        assertEquals("No Match", third_job.getLocation(), "Desert");
+        assertEquals("No Match", third_job.getPositionType(), "Quality control");
+        assertEquals("No Match", third_job.getCoreCompetency(), "Persistence");
     }
 
 }
